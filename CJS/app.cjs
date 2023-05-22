1c51cc3b586fb58f7547992a760b8420c4266fd2
Swrich <fwnevercry@gmail.com>
2023-05-22 12:48:43 +0800

    unkonw

--- CJS/app.cjs
+++ CJS/app.cjs
@@ -1,44 +1,26 @@
-const cron = require('node-cron');
 const { exec } = require('child_process');
 const fs = require('fs');
-const { checkLock, setLock, clearLock } = require('./utils/taskLock.cjs');
+const path = require('path');
 
-const taskList = [];
+const dirPath = path.resolve(__dirname, './shell');
 
-// 读取配置文件
-const config = JSON.parse(fs.readFileSync('scripts.json', 'utf8'));
-
-const execTask = (filename) => {
-  console.log(`Running script ${filename}...`);
-  // 使用child_process模块执行脚本
-  exec(`bash ./shell/${filename}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
-};
-
-// 循环遍历配置文件中的脚本和cron表达式
-config.forEach(({ cron: cronExpression, script: scriptPath, immdiate }) => {
-  const taskName = `!!${scriptPath}!!`;
-
-  if (immdiate) {
-    execTask(scriptPath)
-  }
-  if (checkLock(taskName)) {
-    // 如果有同一任务在执行，则不再执行
-    console.log(`${taskName} is running, skip this time.`);
+fs.readdir(dirPath, (err, files) => {
+  if (err) {
+    console.error(err);
     return;
   }
-  // 设置锁，避免重复执行任务
-  setLock(taskName);
-  // 使用cron模块定时执行脚本
-  cron.schedule(cronExpression, () => {
-    execTask(scriptPath);
-    // 清除锁
-    clearLock(taskName);
-  });
+  files
+    .filter((file) => path.extname(file) === '.sh')
+    .forEach((file) => {
+      const filePath = path.join(dirPath, file);
+      console.log(filePath);
+      exec(`bash ${filePath}`, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+      });
+    });
 });
